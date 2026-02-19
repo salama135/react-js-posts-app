fb1813f8d59e51536419763da9ee1fd640260b1b
Ahmed Salama <Ahmed.Salama@integrant.com>
2026-02-19 14:19:29 +0200

    refactor: refactor route definitions

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import App from './App'
-import NewPost from './components/NewPost';
-import './index.css'
+import Posts from './routes/Posts';
+import NewPost from './routes/NewPost';
+import RootLayout from './routes/RootLayout';
+import './index.css';
 
 const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/create-post', element: <NewPost /> }
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Posts />,
+        children: [{ path: '/create-post', element: <NewPost /> }],
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
