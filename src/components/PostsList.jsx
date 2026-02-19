449c33d46aa7f63f47c8a7c6bb48d699e54e583c
Ahmed Salama <Ahmed.Salama@integrant.com>
2026-02-19 14:19:16 +0200

    feat: handle form submission

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,48 +5,15 @@ import NewPost from './NewPost';
 import Modal from './Modal';
 import classes from './PostsList.module.css';
 
-function PostsList() {
-  const [modalIsVisible, setModalIsVisible] = useState(true);
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
-
-  function hideModalHandler() {
-    setModalIsVisible(false);
-  }
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  // let modalContent;
-
-  // if (modalIsVisible) {
-  //   modalContent = (
-  // <Modal onClose={hideModalHandler}>
-  //   <NewPost
-  //     onBodyChange={bodyChangeHandler}
-  //     onAuthorChange={authorChangeHandler}
-  //   />
-  // </Modal>
-  //   );
-  // }
-
+function PostsList({ isPosting, onStopPosting }) {
   return (
     <>
-      {modalIsVisible && (
-        <Modal onClose={hideModalHandler}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-          />
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Check out the full course!" />
       </ul>
     </>
